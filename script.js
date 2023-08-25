420bb6b275584298a0abd11a050afce5d7a89107
Chims <chimajunior14@gmail.com>
2023-08-25 17:26:46 +0100

    adding a new task

--- script.js
+++ script.js
@@ -2,3 +2,22 @@
 const taskInput = document.getElementById('taskInput');
 const addTaskButton = document.getElementById('addTaskButton');
 const taskList = document.getElementById('taskList');
+
+// Function to add a new task
+function addTask() {
+    const taskText = taskInput.value.trim();
+    
+    if (taskText !== '') {
+        const li = document.createElement('li');
+        li.innerHTML = `
+            <span>${taskText}</span>
+            <button class="deleteButton">Delete</button>
+        `;
+        taskList.appendChild(li);
+        taskInput.value = '';
+        
+        // Attach event listener to delete button
+        const deleteButton = li.querySelector('.deleteButton');
+        deleteButton.addEventListener('click', deleteTask);
+    }
+}
